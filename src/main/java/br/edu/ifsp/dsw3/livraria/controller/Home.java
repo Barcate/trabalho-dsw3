ac5b649563f8f5ec7428a2171eaf2e7e3b795c9f
Matheus Barbosa <mateusbosa21@gmail.com>
2024-09-25 11:17:54 -0300

    tudo certo e apresentado

--- src/main/java/br/edu/ifsp/dsw3/livraria/controller/Home.java
+++ src/main/java/br/edu/ifsp/dsw3/livraria/controller/Home.java
@@ -1,14 +1,20 @@
 package br.edu.ifsp.dsw3.livraria.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.edu.ifsp.dsw3.livraria.model.dao.LivroDAO;
 
 @Controller
 
 public class Home {
-
+    @Autowired
+    LivroDAO ldao;
     @GetMapping(name = "/.")
-    public String principal(){
+    public String principal(ModelMap map){
+        map.addAttribute("livros",ldao.findAll());
         return "/home";
-    }
+    };
 }
